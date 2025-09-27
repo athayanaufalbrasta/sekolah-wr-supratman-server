6b7e3758177ab739238ec7eadb356b9eae43c287
Athaya Naufal Brasta <athayanaufalbrasta@gmail.com>
2025-09-27 12:52:07 +0700

    fix : forgot to staged this file

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 import express from "express";
 import morgan from "morgan";
 import { config } from "dotenv";
+import prisma from "./src/config/db.js";
 import cors from "cors";
-
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerOptions from "./src/config/swaggerDef.js";
 
 config();
 const PORT = process.env.PORT || 3000;
 const app = express();
+const swaggerSpecs = swaggerJsdoc(swaggerOptions);
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 app.use((err, req, res, next) => {
 	res.json({
@@ -24,7 +29,22 @@ app.get("/", async (req, res) => {
 	res.json("Server is running!");
 });
 
+app.get("/test-db", async (req, res) => {
+	try {
+		const result = await prisma.konten_web.findFirst({
+			where: {
+				konten_key: "test_db",
+			},
+		});
+		res.json(result);
+	} catch (err) {
+		res.json({
+			message: err.message,
+		});
+	}
+});
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Dokumentasi API tersedia di http://localhost:${PORT}/api-docs`);
 });
